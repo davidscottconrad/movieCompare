32673cda885f6617ab3018ac5b845d413ba067d4
David Conrad <david.scott.conrad@gmail.com>
2020-08-25 19:33:49 -0400

    Add RunComparison
    Added Logic to on Movie select: check if both sides of the autocomplete are selected. Add runComparison to set up for comparing logic.

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ createAutoComplete({
     root: document.querySelector('#left-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#left-summary'));
+        onMovieSelect(movie, document.querySelector('#left-summary'), 'left');
     },
 });
 
@@ -38,11 +38,14 @@ createAutoComplete({
     root: document.querySelector('#right-autocomplete'),
     onOptionSelect(movie){
         document.querySelector('.tutorial').classList.add('is-hidden');
-        onMovieSelect(movie, document.querySelector('#right-summary'));
+        onMovieSelect(movie, document.querySelector('#right-summary'), 'right');
     },
 });
 
-const onMovieSelect = async (movie, summaryElement) => {
+let leftMovie;
+let rightMovie;
+
+const onMovieSelect = async (movie, summaryElement, side) => {
     const response = await axios.get('http://www.omdbapi.com/', {
         params: {
             apikey: '2625b89b',
@@ -50,11 +53,22 @@ const onMovieSelect = async (movie, summaryElement) => {
         }
     });
     summaryElement.innerHTML = movieTemplate(response.data);
+
+    if (side === 'left'){
+        leftMovie = response.data;
+    }
+    else{
+        rightMovie = response.data;
+    }
+
+    if(leftMovie && rightMovie){
+        runComparison();
+    }
 };
 
-function newfun(){
-    console.log('hello');
-}
+const runComparison = () => {
+    console.log('time for comparison');
+};
 
 const movieTemplate = (movieDetail) => {
     return `
